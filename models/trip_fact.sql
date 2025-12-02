8e5f80fe46793ebd93af9ae415e5513c6d9faebc
Sekou DICKO <sekoudickosyd@gmail.com>
2025-12-02 01:47:40 +0000

    trip_fact model created

---
+++ models/trip_fact.sql
@@ -0,0 +1,16 @@
+with trips as (
+    select 
+        ride_id,
+        RIDEABLE_TYPE,
+        DATE(TO_TIMESTAMP(STARTED_AT)) as trip_date,
+        START_STATIO_ID as START_STATION_ID,
+        END_STATION_ID,
+        MEMBER_CSUAL as MEMBER_CASUAL,
+        TIMESTAMPDIFF(SECOND, TO_TIMESTAMP(STARTED_AT), TO_TIMESTAMP(ENDED_AT)) as trip_duration_sec
+
+    from {{ source('demo', 'bike') }}
+    where ride_id != 'ride_id'
+)
+
+select *
+from trips
